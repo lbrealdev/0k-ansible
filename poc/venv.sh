96b71fc573fda1b8d597651ff2a06c78b42133fd
lbrealdev <lbrealdeveloper@gmail.com>
2023-10-05 08:44:12 +0200

    feat: add comments in venv.sh script and new condition for disable_venv function

--- poc/venv.sh
+++ poc/venv.sh
@@ -12,10 +12,16 @@ function pip_packages() {
 }
 
 function disable_venv() {
+  # If environment variable VIRTUAL_ENV is set and not empty, disable venv and delete it.
   if [ -n "$VIRTUAL_ENV" ]; then
     echo "Deleted $PYTHON_DEFAULT_VENV_NAME with Python 3.12.0 ..."
     deactivate
     rm -rf "./$PYTHON_DEFAULT_VENV_NAME"
+  # If the VIRTUA_LENV environment variable is not set and the venv directory exists, delete venv directory.
+  elif [ ! -v "$VIRTUAL_ENV" ] && [ -e "$PYTHON_DEFAULT_VENV_NAME" ]; then
+    echo "Cleaning up the $PYTHON_DEFAULT_VENV_NAME directory ..."
+    rm -rf "./$PYTHON_DEFAULT_VENV_NAME"
+  # If none of the above conditions, display this message and exit 
   else
     echo "Nothing to see here!"
   fi
